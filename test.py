49227b406efbc4a91a5dc2a46e5846145a720430
Masaki Murata <masaki-murata@umin.ac.jp>
2018-02-16 12:39:35 +0900

    no message

--- test.py
+++ test.py
@@ -46,23 +46,29 @@ voxel_size = np.array([z_min/32, y_min/256, x_min/256])
 #voxelsize.close()
 
 def resize_all(ref_size=np.array([3.6, 0.625, 0.625])):
+    path_to_dir = "../TrainingData_Part%d/"
+    path_to_original_scale_npy = "../IntermediateData/original_scale/%s.npy"
+    path_to_rescaled_npy = "../IntermediateData/rescaled/%s.npy"
     for part in range(1,4):
         for file in os.listdir(path_to_dir % part):
             if re.match("Case[0-9][0-9].mhd", file):
                 volume = readmhd.read(path_to_dir % part + file)
+                np.save(path_to_original_scale_npy % file[:-4], np.float32(volume.vol))
                 matrixsize_rescaled = (volume.matrixsize[::-1]*(volume.voxelsize[::-1] / ref_size)).astype(np.int)
                 matrixsize_rescaled[1]=matrixsize_rescaled[2]
                 volume_rescaled = resize(volume.vol, matrixsize_rescaled)
 #                np.float32(volume_rescaled).tofile("../IntermediateData/rescaled/"+file[:-4]+".raw")
-                np.save("../IntermediateData/rescaled/"+file[:-4]+".npy", np.float32(volume_rescaled))
+                np.save(path_to_rescaled_npy % file[:-4], np.float32(volume_rescaled))
                 print(file, volume_rescaled.shape)
             if re.match("Case[0-9][0-9]_segmentation.mhd", file):
                 volume = readmhd.read(path_to_dir % part + file)
+                np.save(path_to_original_scale_npy % (file[:-4]+"32"), np.float32(volume.vol))
                 matrixsize_rescaled = (volume.matrixsize[::-1]*(volume.voxelsize[::-1] / ref_size)).astype(np.int)
                 matrixsize_rescaled[1]=matrixsize_rescaled[2]
                 volume_rescaled = resize(np.float32(volume.vol), matrixsize_rescaled)
 #                volume_rescaled.tofile("../IntermediateData/rescaled/"+file[:-4]+".raw")
-                np.save("../IntermediateData/rescaled/"+file[:-4]+".npy", np.int8(volume_rescaled))
+                np.save(path_to_rescaled_npy % file[:-4], np.int8(volume_rescaled))
+                np.save(path_to_rescaled_npy % (file[:-4]+"32"), np.float32(volume_rescaled))
                 print(file, volume_rescaled.shape)
     
 def crop_3d(crop_shape=np.array([32,256,256]),
@@ -72,6 +78,7 @@ def crop_3d(crop_shape=np.array([32,256,256]),
     path_to_save_dir = "../IntermediateData/cropped/"
     path_to_mri_cropped = path_to_save_dir + "Case%02d.npy"
     path_to_segmentation_cropped = path_to_save_dir + "Case%02d_segmentation.npy"
+    path_to_segmentation32_cropped = path_to_save_dir + "Case%02d_segmentation32.npy"
     
     if not os.path.isdir(path_to_save_dir):
         os.mkdir(path_to_save_dir)
@@ -86,8 +93,13 @@ def crop_3d(crop_shape=np.array([32,256,256]),
         max_pos = min_pos + crop_shape
         mri_cropped = mri[min_pos[0]:max_pos[0], min_pos[1]:max_pos[1], min_pos[2]:max_pos[2]]
         segmentation_cropped = segmentation[min_pos[0]:max_pos[0], min_pos[1]:max_pos[1], min_pos[2]:max_pos[2]]
-        np.save(path_to_mri_cropped % case, mri_cropped)
-        np.save(path_to_segmentation_cropped % case, segmentation_cropped)
+        if not os.path.exists(path_to_mri_cropped % case):
+            np.save(path_to_mri_cropped % case, mri_cropped)
+        if not os.path.exists(path_to_segmentation_cropped % case):
+            np.save(path_to_segmentation_cropped % case, segmentation_cropped)
+        if not os.path.exists(path_to_segmentation32_cropped % case):
+            np.save(path_to_segmentation32_cropped % case, np.float32(segmentation_cropped))
+        
 
 
 def make_data_label(crop_shape=np.array([32,256,256])):
@@ -110,7 +122,7 @@ def make_data_label(crop_shape=np.array([32,256,256])):
     np.save(path_to_target, label)
     
 
-def predict(path_to_image, path_to_target, model_path, batch_size, path_to_output):
+def prediction(path_to_image, path_to_target, model_path, batch_size, path_to_output):
     # X_sketchが元の病理画像。[0,1]に規格化されたnp array
     X_sketch_train = np.load(path_to_image)
     # X_fullがsegmentationされた画像。[0,1]に規格化された4channel np array
@@ -126,21 +138,32 @@ def predict(path_to_image, path_to_target, model_path, batch_size, path_to_outpu
 
 #    opt_generator = Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 
-    model = seunet(img_dim0, img_dim)
+    generator_model = seunet(img_dim0, img_dim)
     
-    model.load_weights(model_path)
+    print("load weights")
+    generator_model.load_weights(model_path)
     
-    Y_train = model.predict(X_sketch_train, batch_size)
+    print("start prediction")
+    Y_train = generator_model.predict(X_sketch_train, batch_size)
     
     np.save(path_to_output, Y_train.reshape(Y_train.shape[:-1]))
 
-#resize_all(ref_size=voxel_size)
+def make_raw_output(path_to_output, path_to_segmentation_predicted):
+    output = np.float32(np.load(path_to_output))
+    for case in range(50):
+        np.save(path_to_segmentation_predicted % case, output[case])
+        
+resize_all(ref_size=voxel_size)
 #crop_3d()
 #make_data_label()
 
-path_to_image = "../IntermediateData/data_for_train/data.npy"
-path_to_target = "../IntermediateData/data_for_train/label.npy"
-model_path = "../IntermediateData/model/seunet_weights_30.h5"
-batch_size = 1
-path_to_output = "../IntermediateData/output/segmentation_prediction.npy"
-predict(path_to_image, path_to_target, model_path, batch_size, path_to_output)
+#path_to_image = "../IntermediateData/data_for_train/data.npy"
+#path_to_target = "../IntermediateData/data_for_train/label.npy"
+#model_path = "../IntermediateData/model/seunet_weights_30.h5"
+#batch_size = 1
+#path_to_output = "../IntermediateData/output/segmentation_prediction.npy"
+#prediction(path_to_image, path_to_target, model_path, batch_size, path_to_output)
+        
+#path_to_output = "../IntermediateData/output/segmentation_prediction.npy"
+#path_to_segmentation_predicted = "../IntermediateData/cropped/Case%02d_segmentation_predicted.npy"
+#make_raw_output(path_to_output, path_to_segmentation_predicted)
